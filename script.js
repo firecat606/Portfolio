0137651152942fc3a46748a73ef69382dcd04a6e
Firecat <firecat12012@gmail.com>
2024-09-09 15:56:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAboutMe() {
+	window.location.href = "./AboutMe.html";
+}
+
+function showProjects() {
+	alert('Show projects clicked');
+}
+
+function goHome() {
+	window.location.href = "./MainMenu.html";
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('aboutMeButton').addEventListener('click', showAboutMe);
+	document.getElementById('projectsButton').addEventListener('click', showProjects);
+	document.getElementById('homeButton').addEventListener('click', goHome);
+});
